2768528b8408bf5621b503a9bbecd89b2a01d879
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-08 06:13:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ ETFS            = ["TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV"]
 # Option B (new) – Equity Sectors
 OPTION_B_ETFS   = [
     "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV",
-    "XLI", "XLY", "XLP", "XLU", "GDX", "XLB", "XLRE", "XME", "IWM", "IWF", "XSD", "XBI"
+    "XLI", "XLY", "XLP", "XLU", "GDX", "XLB", "XLRE", "XME", "IWM", "IWF", "XSD", "XBI", "IWD", "IWO",
 ]
 
 # Benchmarks (common)
